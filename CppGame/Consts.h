f61bd356d75d64b10e061ceb5e497b8bbfe04567
SHIN YONG JUN <4rest528@naver.com>
2024-08-28 18:52:18 +0900

    change image name

--- CppGame/Consts.h
+++ CppGame/Consts.h
@@ -5,29 +5,29 @@
 namespace Consts
 {
 	const std::string paths[]{ // 이미지 파일 절대 경로
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\1.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\2.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\13.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\14.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\16.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\17.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\20.png)",
-
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\3.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\4.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\5.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\6.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\7.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\8.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\9.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\10.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\11.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\12.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\15.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\18.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\19.png)",
-		R"(C:\Users\4rest\Desktop\Shin\QtGame\threeMatchImage\PNG\ico\21.png)",
-		
+		// used
+		R"(..\threeMatchImage\PNG\ico\1.png)",
+		R"(..\threeMatchImage\PNG\ico\2.png)",
+		R"(..\threeMatchImage\PNG\ico\13.png)",
+		R"(..\threeMatchImage\PNG\ico\14.png)",
+		R"(..\threeMatchImage\PNG\ico\16.png)",
+		R"(..\threeMatchImage\PNG\ico\17.png)",
+		R"(..\threeMatchImage\PNG\ico\20.png)",
+		// unused
+		R"(..\threeMatchImage\PNG\ico\3.png)",
+		R"(..\threeMatchImage\PNG\ico\4.png)",
+		R"(..\threeMatchImage\PNG\ico\5.png)",
+		R"(..\threeMatchImage\PNG\ico\6.png)",
+		R"(..\threeMatchImage\PNG\ico\7.png)",
+		R"(..\threeMatchImage\PNG\ico\8.png)",
+		R"(..\threeMatchImage\PNG\ico\9.png)",
+		R"(..\threeMatchImage\PNG\ico\10.png)",
+		R"(..\threeMatchImage\PNG\ico\11.png)",
+		R"(..\threeMatchImage\PNG\ico\12.png)",
+		R"(..\threeMatchImage\PNG\ico\15.png)",
+		R"(..\threeMatchImage\PNG\ico\18.png)",
+		R"(..\threeMatchImage\PNG\ico\19.png)",
+		R"(..\threeMatchImage\PNG\ico\21.png)",
 	};
 	const int BOARD_LENGTH = 12;
 	const int BOARD_ITEM_SIZE = 60;
